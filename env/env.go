bfced092680dacf14cab2b70df07553315b7fc00
Oleksii Velychko <oleksiivelychko@icloud.com>
2021-11-18 07:45:29 +0200

    change expiration time type

--- env/env.go
+++ env/env.go
@@ -24,12 +24,12 @@ func GetISS() string {
 	return os.Getenv("ISSUER_ISS")
 }
 
-func GetEXP() int {
+func GetEXP() int64 {
 	expirationTimeExp := os.Getenv("EXPIRATION_TIME_EXP")
 	if expirationTimeExp != "" {
-		exp, err := strconv.ParseInt(expirationTimeExp, 10, 32)
+		exp, err := strconv.ParseInt(expirationTimeExp, 10, 64)
 		if err == nil {
-			return int(exp)
+			return exp
 		}
 	}
 	return 60
