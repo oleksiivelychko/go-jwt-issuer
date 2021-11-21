34d715e64421b86bb76034b910d7f7307a90c189
Oleksii Velychko <oleksiivelychko@icloud.com>
2021-11-21 11:04:30 +0200

    add test to validate issued token

--- issuer/issuer_test.go
+++ issuer/issuer_test.go
@@ -1,18 +1,40 @@
 package issuer
 
 import (
+	"github.com/golang-jwt/jwt"
+	"github.com/oleksiivelychko/go-jwt-issuer/env"
+	"os"
 	"testing"
 )
 
-func TestIssueJWT(t *testing.T) {
-	_, _, _, err := IssueUserJWT(0)
+func TestIssueAndValidateToken(t *testing.T) {
+	_ = os.Setenv("SECRET_KEY", "secretkey")
+	_ = os.Setenv("EXPIRES_MINUTES", "10")
+
+	var secretKey = env.GetSecretKey()
+	var aud = env.GetAUD()
+	var iss = env.GetISS()
+	var expiresMinutes = env.GetExpiresMinutes()
+
+	token, _, exp, err := IssueUserJWT(secretKey, aud, iss, expiresMinutes, 1)
 	if err != nil {
-		t.Errorf("failed to get the complete signed token: %s", err.Error())
+		t.Errorf("failed to get signed token: %s", err.Error())
+	}
+
+	jwtClaims, err := ValidateToken(token, secretKey, aud, iss, exp)
+	if _, ok := jwtClaims.Claims.(jwt.MapClaims); !ok || !jwtClaims.Valid {
+		t.Errorf("failed to validate signed token: %s", err.Error())
 	}
 }
 
 func BenchmarkIssueJWT(b *testing.B) {
+	var secretKey = env.GetSecretKey()
+	var aud = env.GetAUD()
+	var iss = env.GetISS()
+	var expiresMinutes = env.GetExpiresMinutes()
+
 	for i := 0; i < b.N; i++ {
-		_, _, _, _ = IssueUserJWT(0)
+		token, _, exp, _ := IssueUserJWT(secretKey, aud, iss, expiresMinutes, 1)
+		_, _ = ValidateToken(token, secretKey, aud, iss, exp)
 	}
 }
