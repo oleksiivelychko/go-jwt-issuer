fd7ab8b703b1abb833f9929f50be9de6b17551e6
Oleksii Velychko <oleksiivelychko@icloud.com>
2021-11-28 08:54:13 +0200

    create handlers package

--- main.go
+++ main.go
@@ -1,28 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"github.com/oleksiivelychko/go-jwt-issuer/env"
+	"github.com/oleksiivelychko/go-jwt-issuer/handlers"
 	"github.com/oleksiivelychko/go-jwt-issuer/service"
 	"log"
 	"net/http"
 )
-
-func issueAccessTokenHandler(tokenService *service.Service) func(w http.ResponseWriter, r *http.Request) {
-	if tokenService.Redis == nil {
-		log.Fatal("cannot established redis connection")
-	}
-
-	return func(w http.ResponseWriter, r *http.Request) {
-		accessToken, refreshToken, exp, err := tokenService.GenerateUserTokenPair(1)
-		if err != nil {
-			_, _ = fmt.Fprintf(w, "failed to get access token: %s", err.Error())
-		}
-		_, _ = fmt.Fprintf(w, "access-token: %s \n", accessToken)
-		_, _ = fmt.Fprintf(w, "refresh-token: %s \n", refreshToken)
-		_, _ = fmt.Fprintf(w, "expiration-time: %d \n", exp)
-	}
-}
 
 func main() {
 	cfg := env.InitConfig()
@@ -31,7 +15,7 @@ func main() {
 		Redis: cfg.InitRedis(),
 	}
 
-	http.HandleFunc("/", issueAccessTokenHandler(&tokenService))
-	http.HandleFunc("/access-token", issueAccessTokenHandler(&tokenService))
+	http.HandleFunc("/", handlers.AccessTokenHandler(&tokenService))
+	http.HandleFunc("/access-token", handlers.AccessTokenHandler(&tokenService))
 	log.Fatal(http.ListenAndServe(env.GetPort(), nil))
 }
