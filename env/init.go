a8bc310dff38f357ff5e4e59ae9ad4dcb1f58d4c
Oleksii Velychko <oleksiivelychko@icloud.com>
2021-11-28 13:39:32 +0200

    change expires minutes default value

--- env/init.go
+++ env/init.go
@@ -14,7 +14,7 @@ func InitEnv() {
 	_ = os.Setenv("REDIS_PASSWORD", "secret")
 	_ = os.Setenv("REDIS_DB", "0")
 	_ = os.Setenv("SECRET_KEY", "secretkey")
-	_ = os.Setenv("EXPIRES_MINUTES", "10")
+	_ = os.Setenv("EXPIRES_MINUTES", "5")
 	_ = os.Setenv("AUDIENCE_AUD", "account.jwt.local")
 	_ = os.Setenv("ISSUER_ISS", "jwt.local")
 }
