56540453a70bc33f2ce18989ca6829a8702fc4c7
Oleksii Velychko <oleksiivelychko@icloud.com>
2021-12-06 15:48:43 +0200

    extend json jwt structure with error fields

--- env/init.go
+++ env/init.go
@@ -6,15 +6,18 @@ type JsonJwt struct {
 	AccessToken    string `json:"access-token"`
 	RefreshToken   string `json:"refresh-token"`
 	ExpirationTime string `json:"expiration-time"`
+	ErrorMessage   string `json:"error-message"`
+	ErrorCode      uint8  `json:"error-code"`
 }
 
 func InitEnv() {
+	_ = os.Setenv("PORT", "8080")
 	_ = os.Setenv("REDIS_HOST", "localhost")
 	_ = os.Setenv("REDIS_PORT", "6379")
 	_ = os.Setenv("REDIS_PASSWORD", "secret")
 	_ = os.Setenv("REDIS_DB", "0")
 	_ = os.Setenv("SECRET_KEY", "secretkey")
-	_ = os.Setenv("EXPIRES_MINUTES", "5")
+	_ = os.Setenv("EXPIRES_MINUTES", "1")
 	_ = os.Setenv("AUDIENCE_AUD", "account.jwt.local")
 	_ = os.Setenv("ISSUER_ISS", "jwt.local")
 }
