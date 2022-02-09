da0c858e1265377648354919711ce904013c7a44
Oleksii Velychko <oleksiivelychko@icloud.com>
2022-02-09 06:57:03 +0200

    create optimized server

--- main.go
+++ main.go
@@ -1,24 +1,53 @@
 package main
 
 import (
+	"context"
 	"github.com/oleksiivelychko/go-jwt-issuer/env"
 	"github.com/oleksiivelychko/go-jwt-issuer/handlers"
 	"github.com/oleksiivelychko/go-jwt-issuer/service"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 )
 
 func main() {
-	// env.InitEnv() // uncomment for local testing
+	//env.InitEnv() // uncomment for local testing
 	cfg := env.InitConfig()
 	tokenService := service.Service{
 		Env:   cfg,
 		Redis: cfg.InitRedis(),
 	}
 
-	http.HandleFunc("/access-token/", handlers.AccessTokenHandler(&tokenService))
-	http.HandleFunc("/refresh-token/", handlers.RefreshTokenHandler(&tokenService))
-	http.HandleFunc("/clear-token/", handlers.ClearTokenHandler(&tokenService))
-	http.HandleFunc("/authorize-token/", handlers.AuthorizeTokenHandler(&tokenService))
-	log.Fatal(http.ListenAndServe(env.GetPort(), nil))
+	serveMux := http.NewServeMux()
+	serveMux.Handle("/access-token/", handlers.NewAccessTokenHandler(&tokenService))
+	serveMux.Handle("/refresh-token/", handlers.NewRefreshTokenHandler(&tokenService))
+	serveMux.Handle("/clear-token/", handlers.NewClearTokenHandler(&tokenService))
+	serveMux.Handle("/authorize-token/", handlers.NewAuthorizeTokenHandler(&tokenService))
+
+	server := &http.Server{
+		Addr:         env.GetPort(),
+		Handler:      serveMux,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	signalChannel := make(chan os.Signal)
+	signal.Notify(signalChannel, os.Interrupt)
+	signal.Notify(signalChannel, os.Kill)
+
+	sig := <-signalChannel
+	log.Println("Received terminate, graceful shutdown", sig)
+
+	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	server.Shutdown(ctx)
 }
